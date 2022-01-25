8cffcf20534fe3c5b6f1ce512472b8d59e8f1bba
Md.Usman Ansari <40880948+MdUsmanAnsari@users.noreply.github.com>
2022-01-25 23:47:27 +0530

    add: some animations

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 const root = document.querySelector('.wrapper');
 const input = document.querySelector('input');
-const button = document.querySelector('button')
+const button = document.querySelector('button');
+const ansDropDown = document.querySelector('.select-animation');
 
-let TEXT = input.value.toUpperCase();
+
+let TEXT = input.value.toUpperCase()
 const COLORS = ['#ff3838', '#fff200', '#25CCF7', '#c56cf0', '#ff9f1a', '#ffb8b8', '#FAFAFA']
+let animationName = ansDropDown[0].value
 
 const createElement = (elementName, className) => {
       const element = document.createElement(elementName)
@@ -16,7 +19,7 @@ const createLetters = (parentNode, char, diff) => {
       const LEN_OF_ELEMENT = 8;
       for (let i = 0; i < LEN_OF_ELEMENT; i++) {
             const delay = ((LEN_OF_ELEMENT - i + 1) / 10) + diff;
-            const element = createElement('span', 'letter');
+            const element = createElement('span', `letter ${animationName}`);
             element.textContent = char;
             element.style = `--delay:${delay}s; color:${COLORS[i]};`
             parentNode.append(element)
@@ -47,6 +50,11 @@ input.addEventListener('input', (event) => {
 button.addEventListener('click', () => {
       const isAnimationStop = root.classList.toggle('animation-stop');
       button.textContent = isAnimationStop ? 'START' : 'STOP'
+})
+
+ansDropDown.addEventListener('change', (event) => {
+      animationName = event.target.value;
+      init();
 })
 
 init();
